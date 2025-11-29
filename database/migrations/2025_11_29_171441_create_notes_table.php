94c9c27a6e154d17bfcc7071f62ff195b7e319a2
Jose Prieto <alejandroprietosal@gmail.com>
2025-11-29 13:10:04 -0500

    Estructura base de vistas para Notas y Etiquetas

---
+++ database/migrations/2025_11_29_171441_create_notes_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
+        });
+
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('note_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('note_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('note_tag');
+    }
+};
---
+++ resources/views/tags/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+@section('title', 'Welcome to MyTagNotes')
+
+@section('content')
+    <h1 class="text-center text-2xl font-semibold">Etiquetas</h1>
+@endsection
